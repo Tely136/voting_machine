5ff33445ad10d78558db21d8ce6a21941f999a16
Thomas <tely000@citymail.cuny.edu>
2024-11-06 18:13:51 -0500

    added seperate files for admin and voter functions

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,22 @@
+mod admin;
+mod utils;
+
 use std::{
     // env,
-    error::Error, fs, io::{self, Write}, path, process
+    // error::Error,
+    // fs,
+    io::{self, Write},
+    // path,
+    // process
 };
-use csv;
 
 
-fn read_input() -> String {
-    let mut input = String::new();
-    io::stdin()
-        .read_line(&mut input)
-        .unwrap_or_else(|err| {
-            eprintln!("Error reading input: {}", err);
-            std::process::exit(1);
-        });
-    input.trim().to_string()
+fn voter_loop() {
+
 }
 
 
-fn main() {
+fn admin_loop() {
     loop {
         println!("Admin interface");
         println!("Enter 1 to create new ballot");
@@ -25,95 +24,22 @@ fn main() {
         println!("Enter 3 to register new voters");
         println!("Enter 4 to open or close an election");
         println!("Enter 5 to tally votes for an election");
+        println!("Enter 0 to return to main menu");
         print!("Selection: ");
         _ = io::stdout().flush();
-        let selection = read_input().trim().parse::<i32>().unwrap();
 
+        let selection = utils::read_input().trim().parse::<i32>().unwrap();
         if selection == 1 {
             // get folder name for ballot
             // save csv file with header for name, party, political office
-            print!("Enter ballot name:");
-            _ = io::stdout().flush();
-            let folder_name = read_input();
-
-            // check if folder exists
-            let folder_path = path::Path::new("./").join(&folder_name);
-            if folder_path.try_exists().expect("couldn't check existence") == true { // need to fix the expect
-                println!("Ballot with this name already exists, overwriting");
-                fs::remove_dir_all(&folder_path).unwrap();
-            }
-
-            fs::create_dir(&folder_path).unwrap();
-
-            let candidates_filepath = folder_path.join("candidates.csv");
-            let mut wtr = csv::Writer::from_path(candidates_filepath).unwrap(); // fix the unwrap here
-            wtr.write_record(&["Name", "Party", "Office"]).unwrap();
-            wtr.flush().unwrap();
+            admin::create_ballot();
         }
         else if selection == 2 {
             // get folder name to load ballot from
             // create ballot object using the file
             // loop asking for new candidates to be added to ballot
             // end loop when certain input is entered 
-
-            print!("Enter ballot name:");
-            _ = io::stdout().flush();
-            let folder_name = read_input();
-            println!("");
-
-            let folder_path = path::Path::new("./").join(&folder_name);
-            if folder_path.try_exists().expect("couldn't check existence") == true { // need to fix the expect   
-                loop {
-                    println!("Enter 0 to finish");
-                    print!("Enter candidate name: "); // need error for invalid input that forces user to retry
-                    _ = io::stdout().flush();
-                    let candidate_name = read_input();
-                    if candidate_name == 0.to_string() {
-                        break;
-                    }
-
-                    print!("Enter candidate party: ");
-                    _ = io::stdout().flush();
-                    let candidate_party = read_input();
-                    if candidate_party == 0.to_string() {
-                        break;
-                    }
-
-                    print!("Enter 1 for President, 2 for Senate, 3 for Judge: "); // need error for invalid input that forces user to retry
-                    _ = io::stdout().flush();
-                    let mut candidate_office = String::from("");
-                    let candidate_office_input = read_input();
-                    if candidate_office_input == 1.to_string() {
-                        candidate_office = String::from("President");
-                    }
-                    else if candidate_office_input == 2.to_string() {
-                        candidate_office = String::from("Senate");
-                    }
-                    else if candidate_office_input == 3.to_string() {
-                        candidate_office = String::from("Judge");
-                    }
-                    else if candidate_office_input == 0.to_string() {
-                        break;
-                    }
-                    // else {
-                    //     // try again
-                    // }
-
-                    // write row into ballot.csv file
-                    let candidates_filepath = folder_path.join("candidates.csv");
-                    let file = fs::OpenOptions::new()
-                        .append(true)
-                        .create(true)
-                        .open(&candidates_filepath).unwrap();
-                    let mut wtr = csv::Writer::from_writer(file);
-                
-                    wtr.flush().unwrap();
-                    wtr.write_record(&[candidate_name, candidate_party, candidate_office]).unwrap();
-                }
-            }
-            else {
-                // try again
-            }
+            admin::add_candidate();
         }
         else if selection == 3 {
             // open csv file of registered boters (maybe later this file can be encrypted or something idk)
@@ -127,9 +53,28 @@ fn main() {
         else if selection == 5 {
             
         }
+        else if selection == 0 {
+            break;
+        }
         else {
             println!("Invalid selection");
         }
         println!("");
+    }
+}
+
+
+fn main() {
+    loop {
+        println!("Welcome to the voting machine");
+        println!("***Voter Options***");
+        println!("Admins enter 0 to login");
+
+        let input = utils::read_input();
+        if input == "0" {
+            // if admin_authenticate == true {
+                admin_loop();
+            // }
+        }   
     }
 }
